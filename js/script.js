8e0a2bbc396e71c73a22f3d2a79a160a09b36c42
Oskar <oskaras34@gmail.com>
2020-01-11 22:00:35 +0200

    remaking interview project. Adding OOP and IE 11 support

--- js/script.js
+++ js/script.js
@@ -1,31 +1,78 @@
-const enlargedImage = document.querySelector(".enlarged-image img");
+function ImageLoader() {
+  // @param leftSideGalleryContainer tage where images will be loaded.
+  // Ex: document.querySelector('.gallery')
+  this.loadLeftSide = async function(leftSideGalleryContainer) {
+    let leftSideImages = await fetch('https://picsum.photos/v2/list');
+    let leftSideImagesParsed = await leftSideImages.json();
 
-const images = document.querySelectorAll(".gallery img").forEach(image => {
-  image.addEventListener("click", () => {
+    leftSideImagesParsed.forEach(image => {
+      let galleryImage = document.createElement('img');
+      let imgSrc = 'https://picsum.photos/id/' + image.id + '/200';
+      galleryImage.setAttribute('src', imgSrc);
+      leftSideGalleryContainer.append(galleryImage);
+    });
+  };
+}
+// https://ccoenraets.github.io/es6-tutorial/setup-babel/
+
+let loadImages = new ImageLoader();
+loadImages.loadLeftSide(document.querySelector('.gallery'));
+
+const enlargedImage = document.querySelector('.enlarged-image img');
+
+const images = document.querySelectorAll('.gallery img').forEach(image => {
+  image.addEventListener('click', () => {
     addInfoAndImage(image);
   });
 });
 
 async function addInfoAndImage(clickedImage) {
-    let divWidth = document.querySelector(".right-side").offsetWidth;
-    let divHeight = document.querySelector(".right-side").offsetHeight;
+  let leftSideImages = await fetch('https://picsum.photos/v2/list');
+  let leftSideImagesParsed = await leftSideImages.json();
+  leftSideImagesParsed.forEach(image => {
+    console.log(image);
+  });
 
-    let selectedImage = clickedImage.src;
-    let stringArray = selectedImage.split("/");
-    urlInfo = "https://picsum.photos/id/" + stringArray[4] + "/info";
+  let divWidth = document.querySelector('.right-side').offsetWidth;
+  let divHeight = document.querySelector('.right-side').offsetHeight;
+
+  let selectedImage = clickedImage.src;
+  let stringArray = selectedImage.split('/');
+  urlInfo = 'https://picsum.photos/id/' + stringArray[4] + '/info';
 
   // getting data and adding above image
   let test = await fetch(urlInfo);
   let result = await test.json();
-  
-  const info = document.querySelector("#info");
-  info.innerHTML = "Author: " + result.author + " height: " + result.height + " width: " + result.width;
+
+  const info = document.querySelector('#info');
+  info.innerHTML =
+    'Author: ' +
+    result.author +
+    ' height: ' +
+    result.height +
+    ' width: ' +
+    result.width;
   textHeight = info.offsetHeight;
 
-  // to leave enought space for text with any font-size
-  result.height > result.width ? (divWidth = Math.floor(divHeight / 1.5), divHeight = divHeight - textHeight) : (divHeight = (Math.floor(divWidth / 1.5), divHeight = divHeight - textHeight))
-  
+  if (result.width > result.height) {
+    if (result.width > divWidth) {
+      finalHeight = Math.floor((result.height * divWidth) / result.width);
+      finalWidth = divWidth;
+    }
+  } else {
+    if (result.height > divHeight) {
+      finalWidth = Math.floor((result.width * divHeight) / result.height);
+      finalHeight = divHeight;
+    }
+  }
+
   // stringArray[4] === id
-  enlargedImage.src = "https://picsum.photos/id/" + stringArray[4] + "/" + divWidth + '/' + divHeight
-  enlargedImage.alt = 'enlarged image'
+  enlargedImage.src =
+    'https://picsum.photos/id/' +
+    stringArray[4] +
+    '/' +
+    finalWidth +
+    '/' +
+    finalHeight;
+  enlargedImage.alt = 'enlarged image';
 }
